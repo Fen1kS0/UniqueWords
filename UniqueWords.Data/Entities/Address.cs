162a89604c52f776c8a645b7b10ecd26e2be2187
Fen1kS0 <ffgh04@gmail.com>
2021-03-29 10:19:14 +0400

    Added entities

---
+++ UniqueWords.Data/Entities/Address.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace UniqueWords.Data.Entities
+{
+    public class Address
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public ICollection<Word> UniqueWords { get; set; }
+    }
+}
---
+++ UniqueWords.Data/Entities/Word.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace UniqueWords.Data.Entities
+{
+    public class Word
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        
+        public int Count { get; set; }
+
+        [Required]
+        public Address Address { get; set; }
+    }
+}
